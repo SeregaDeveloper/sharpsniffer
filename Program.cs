e1a67dafbee85c8625659b247360345535dd8102
SeregaDeveloper <48175873+SeregaDeveloper@users.noreply.github.com>
2019-03-21 23:03:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-// (Не) сниффер, создан для прослушки сети и автоматической загрузки нужной информации в БД, для её дальнейшей обработки
-// любыми средствами и на любых языках.
+// Shiffer for Windows 10. 
+// Made for collect IP and TCP packet data into MySQL Database
 
 using System;
 using System.Collections.Generic;
@@ -21,25 +21,27 @@ namespace SharpSniffer
     {
           static void Main(string[] args)
           {
-            var IPv4Addresses = Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(al => al.AddressFamily == AddressFamily.InterNetwork).AsEnumerable();
+            // Get hosts from our network
+              
+              var IPv4Addresses = Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(al => al.AddressFamily == AddressFamily.InterNetwork).AsEnumerable();
 
-            // выясняем какие адреса находятся в сети на данный момент
+            
 
           Console.WriteLine("Protocol\tSourceIP:Port\t===>\tDestinationIP:Port\t---Flag");
 
-            // проходимся по всем адресам и прослушиваем их входящие и исходящие пакеты
+            // Listen each packet to each address 
 
             foreach (IPAddress ip in IPv4Addresses)
                                   Sniff(ip);
 
-            // пока не нажмём любую клавишу
+            // While any the button is not pressed
 
             Console.Read();
             }
 
             public static string ToProtocolString(this byte b)  {
        
-            // выясняем тип протокола
+            // Detect the type of the protocol
             switch (b)
             {
                 case 1: return "ICMP";
@@ -56,20 +58,19 @@ namespace SharpSniffer
             }
           static void Sniff(IPAddress ip)  {
                
-            // запускаем сокет на прослушку протоколов версии 4
+            // Start sniffing for ipv4 protocol packets
 
               Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
               sck.Bind(new IPEndPoint(ip, 0));
               sck.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
               sck.IOControl(IOControlCode.ReceiveAll, new byte[4] { 1, 0, 0, 0 }, null);
 
-            //массив для удержания данных из пакета
-
-            //использую стандартный ip заголовок  (20byte)  + 4 + 4 + 4 +2  для парсинга необходимой части ТСР заголовка
+            // Data Array
+            // Use standart IP header  (20byte) and the part of TCP header (14byte) for parsing
 
             byte[] buffer = new byte[34];
 
-            // используем асинхронную обработку данных
+            
 
             Action<IAsyncResult> OnReceive = null;
                OnReceive = (ar) =>
@@ -79,23 +80,23 @@ namespace SharpSniffer
                    
                      Console.WriteLine("{0}\t{1}:{2}\t===>\t{3}:{4}  ", buffer.Skip(9).First().ToProtocolString() 
 
-                        , new IPAddress(BitConverter.ToUInt32(buffer, 12)).ToString() // сендер
+                        , new IPAddress(BitConverter.ToUInt32(buffer, 12)).ToString() // Sender IP
 
-                        , ((ushort) IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 20))).ToString() // порт с
+                        , ((ushort) IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 20))).ToString() // Sender Port
 
-                         , new IPAddress(BitConverter.ToUInt32(buffer, 16)).ToString() //  ресивер
+                         , new IPAddress(BitConverter.ToUInt32(buffer, 16)).ToString() //  Reciever IP
 
-                        , ((ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 22))).ToString() // порт р
+                        , ((ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 22))).ToString() // Reciever Port
 
 
                          );
                    
-                   buffer = new byte[34]; // очистка буффера
+                   buffer = new byte[34]; // Clean the buffer
 
-                    sck.BeginReceive(buffer, 0, 34, SocketFlags.None, new AsyncCallback(OnReceive), null); // повтор прослушки
+                    sck.BeginReceive(buffer, 0, 34, SocketFlags.None, new AsyncCallback(OnReceive), null); // Repeat listening
                 };     
                
-            // продолжаем прослушку сокетом
+      
                 sck.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None,new AsyncCallback(OnReceive), null);
            }           
     }
